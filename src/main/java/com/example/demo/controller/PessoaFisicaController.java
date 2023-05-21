30be815012d485de106d7263c640a8cca6f12108
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2023-05-21 11:03:33 -0300

    fazendo testes de qualidade

---
+++ src/main/java/com/example/demo/controller/PessoaFisicaController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Controller
+public class PessoaFisicaController {
+
+}
